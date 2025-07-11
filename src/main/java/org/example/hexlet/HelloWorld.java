856f8d96f884d70501977a985eb2d807ff0c81ee
yas-7 <kasmenai@gmail.com>
2025-07-11 21:33:11 +0300

    use params

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,12 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            ctx.result("Hello, " + name +"!");
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
