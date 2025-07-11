07c583edc015b299d62f45eca37b5631fda44727
yas-7 <kasmenai@gmail.com>
2025-07-11 14:58:55 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+	java
+	application
+}
+
+application {
+	mainClass.set("org.example.hexlet.HelloWorld")
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(24)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	dependencies {
+		// Актуальные версии зависимостей
+		implementation("io.javalin:javalin:6.1.3")           // версия фреймворка Javalin
+		implementation("org.slf4j:slf4j-simple:2.0.7")       // простая реализация SLF4J
+		implementation("io.javalin:javalin-rendering:6.1.3") // библиотека для рендеринга шаблонов
+		implementation("gg.jte:jte:3.1.9")                  // JTE — фреймворк для рендеринга шаблонов
+		testImplementation(platform("org.junit:junit-bom:5.9.1")) // Тестовая инфраструктура JUnit
+		testImplementation("org.junit.jupiter:junit-jupiter") // Библиотека юнит-тестов JUnit Jupiter
+	}
+}
+
+tasks.test {
+	useJUnitPlatform()
+	// https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		// showStackTraces = true
+		// showCauses = true
+		showStandardStreams = true
+	}
+}
